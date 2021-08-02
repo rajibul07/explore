c6d904ce44651bd93b546de1ad87e0caccdecd73
rajibul07 <74898533+rajibul07@users.noreply.github.com>
2021-08-02 21:06:02 +0530

    Update sum.java

--- PractiseDsa/sum.java
+++ PractiseDsa/sum.java
@@ -1,15 +1,15 @@
 
 //Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target
-
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-    for(int i=0;i<nums.length;i++){
-        for(int j=i+1;j<nums.length;j++){
-            if(nums[i]+nums[j]==target){
-                return new int[] {i,j};
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    return new int[] { i, j };
+                }
             }
         }
-    }
+        // In case there is no solution, we'll just return null
         return null;
     }
 }
